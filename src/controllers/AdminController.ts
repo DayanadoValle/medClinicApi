9fac4b50cea920da2e51fea079b3d654c3fc8e59
DayanadoValle <dayanadovalle@hotmail.com>
2026-09-14 20:31:30 -0300

    feat: cria controller administrativo

---
+++ src/controllers/AdminController.ts
@@ -0,0 +1,11 @@
+import { Response } from "express";
+import { AuthenticatedRequest } from "../middlewares/authMiddleware";
+
+export class AdminController {
+  ping = (req: AuthenticatedRequest, res: Response): void => {
+    res.status(200).json({
+      message: "Acesso autorizado: voce esta logado como Administrador.",
+      userId: req.user?.id,
+    });
+  };
+}
