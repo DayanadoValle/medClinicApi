9af965de88bc47b378d012029976c9ac7507a6ef
DayanadoValle <dayanadovalle@hotmail.com>
2026-09-14 20:05:52 -0300

    feat: cria DTOs de usuario e autenticacao

---
+++ src/dtos/user.dto.ts
@@ -0,0 +1,26 @@
+import { UserRole } from "../entities/UserRole";
+
+export interface CreateUserDTO {
+  name: string;
+  email: string;
+  password: string;
+  role?: UserRole;
+}
+
+export interface LoginDTO {
+  email: string;
+  password: string;
+}
+
+export interface UserResponseDTO {
+  id: string;
+  name: string;
+  email: string;
+  role: UserRole;
+  createdAt: Date;
+}
+
+export interface AuthResponseDTO {
+  token: string;
+  user: UserResponseDTO;
+}
