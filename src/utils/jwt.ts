f28593358999650569ad62caa4bd95cdb0fbddcc
DayanadoValle <dayanadovalle@hotmail.com>
2026-09-14 20:00:24 -0300

    feat: cria utilitario de JWT

---
+++ src/utils/jwt.ts
@@ -0,0 +1,22 @@
+import jwt, { SignOptions } from "jsonwebtoken";
+
+import { env } from "../config/env";
+
+import { UserRole } from "../entities/UserRole";
+
+export interface TokenPayload {
+  id: string;
+  role: UserRole;
+}
+
+export function generateToken(payload: TokenPayload): string {
+  const options: SignOptions = {
+    expiresIn: env.jwt.expiresIn as SignOptions["expiresIn"],
+  };
+
+  return jwt.sign(payload, env.jwt.secret, options);
+}
+
+export function verifyToken(token: string): TokenPayload {
+  return jwt.verify(token, env.jwt.secret) as TokenPayload;
+}
